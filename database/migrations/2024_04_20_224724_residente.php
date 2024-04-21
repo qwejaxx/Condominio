b22f092adcdea2545cfb36d2a81f55771562ca14
jachik <ivanrosales395@gmail.com>
2024-04-20 20:38:53 -0400

    Relacion con la tabla users

--- database/migrations/2024_04_20_224724_residente.php
+++ database/migrations/2024_04_20_224724_residente.php
@@ -19,6 +19,22 @@ return new class extends Migration
             $table->string('apellidom_rsdt', 20)->nullable();
             $table->date('fechanac_rsdt');
             $table->string('telefono_rsdt', 20);
+            $table->string('estado_rsdt', 20);
+            $table->unsignedInteger('usuario_id_rsdt')->nullable();
+            $table->foreign('usuario_id_rsdt')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedInteger('rep_fam_id_rsdt')->nullable();
+            $table->foreign('rep_fam_id_rsdt')->references('id_rsdt')->on('residente')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+
+        /* Schema::create('residente', function (Blueprint $table) {
+            $table->increments('id_rsdt');
+            $table->string('ci_rsdt', 15)->unique();
+            $table->string('nombre_rsdt', 20);
+            $table->string('apellidop_rsdt', 20);
+            $table->string('apellidom_rsdt', 20)->nullable();
+            $table->date('fechanac_rsdt');
+            $table->string('telefono_rsdt', 20);
             $table->string('rol_rsdt', 50);
             $table->string('usuario_rsdt', 50)->nullable();
             $table->string('contrasena_rsdt', 255)->nullable();
@@ -26,7 +42,7 @@ return new class extends Migration
             $table->unsignedInteger('rep_fam_id_rsdt')->nullable();
             $table->foreign('rep_fam_id_rsdt')->references('id_rsdt')->on('residente')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
-        });
+        }); */
     }
 
     /**
